ccaefe80c0170e434d8d39fc9f0c6d3b7decfdf1
devGiovanni95 <giovanni.DevJava@hotmail.com>
2021-07-25 19:27:47 -0300

    Criando primeira classe de dominio categorias

--- src/main/java/br/com/projetospring/controllers/CategoriaController.java
+++ src/main/java/br/com/projetospring/controllers/CategoriaController.java
@@ -1,16 +1,27 @@
 package br.com.projetospring.controllers;
 
+import br.com.projetospring.domain.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaController {
 
     @GetMapping
-    public String listar(){
-        return "Rest está funcionando";
+    public List<Categoria> listar(){
+
+        Categoria categoria1 = new Categoria(1,"Informática");
+        Categoria categoria2 = new Categoria(2,"Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(categoria1);
+        lista.add(categoria2);
+        return lista;
     }
 
 }
---
+++ src/main/java/br/com/projetospring/domain/Categoria.java
@@ -0,0 +1,48 @@
+package br.com.projetospring.domain;
+
+import java.util.Objects;
+
+public class Categoria {
+
+    private Integer id;
+    private String nome;
+
+    public Categoria(){
+
+    }
+
+    public Categoria(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Categoria)) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(getId(), categoria.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
