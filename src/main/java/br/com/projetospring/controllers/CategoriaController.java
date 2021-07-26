e73f6437b07efbc4945412deede81ce9f41ef0c1
devGiovanni95 <giovanni.DevJava@hotmail.com>
2021-07-26 20:54:28 -0300

    Criando Repository e Service para Categoria

--- src/main/java/br/com/projetospring/controllers/CategoriaController.java
+++ src/main/java/br/com/projetospring/controllers/CategoriaController.java
@@ -1,27 +1,26 @@
 package br.com.projetospring.controllers;
 
 import br.com.projetospring.entities.Categoria;
+import br.com.projetospring.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaController {
 
+    @Autowired
+    private CategoriaService categoriaService;
+
     @GetMapping
-    public List<Categoria> listar(){
-
-        Categoria categoria1 = new Categoria(1,"Informática");
-        Categoria categoria2 = new Categoria(2,"Escritório");
-
-        List<Categoria> lista = new ArrayList<>();
-        lista.add(categoria1);
-        lista.add(categoria2);
-        return lista;
+    @RequestMapping(value = "/{id}")
+    public ResponseEntity<?> listar(@PathVariable Integer id) {
+        Categoria obj = categoriaService.busca(id);
+        return ResponseEntity.ok().body(obj);
     }
 
 }
---
+++ src/main/java/br/com/projetospring/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package br.com.projetospring.services;
+
+import br.com.projetospring.entities.Categoria;
+import br.com.projetospring.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired//serve para intanciar automaticamente pela jpa
+    private CategoriaRepository categoriaRepository;
+
+    public Categoria busca(Integer id){
+        Optional <Categoria> obj = categoriaRepository.findById(id);
+        return obj.orElse(null);
+    }
+}
