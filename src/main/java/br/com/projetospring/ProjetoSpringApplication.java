22a8580692f8cb875be3efde4967f53f35a4a116
devGiovanni95 <giovanni.DevJava@hotmail.com>
2021-07-26 21:00:54 -0300

    Criando operaçao de intanciacao

--- src/main/java/br/com/projetospring/ProjetoSpringApplication.java
+++ src/main/java/br/com/projetospring/ProjetoSpringApplication.java
@@ -1,13 +1,32 @@
 package br.com.projetospring;
 
+import br.com.projetospring.entities.Categoria;
+import br.com.projetospring.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class ProjetoSpringApplication {
+public class ProjetoSpringApplication implements CommandLineRunner {
+
+	//Para salvar precisamos chamar a classe para salvar
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoSpringApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria categoria1 = new Categoria(null,"Informática");
+		Categoria categoria2 = new Categoria(null,"Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+
+
+	}
 }
