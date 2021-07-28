7bf0995144fd034117e78de1a93761ca63d9d05d
devGiovanni95 <giovanni.DevJava@hotmail.com>
2021-07-27 23:35:12 -0300

    Criando classes de Cliente e endereço

---
+++ src/main/java/br/com/projetospring/entities/Endereco.java
@@ -0,0 +1,112 @@
+package br.com.projetospring.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Endereco implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cep;
+
+    private Cliente cliente;//um endero tem um cliente
+
+    private Cidade cidade;//um endereco é de uma cidade apenas(Mexemos somente aki pois a cidade nao precisa conhecer o endereço mais so o endereço conhece a cidade)
+
+    private Endereco(){
+
+    }
+
+    public Endereco(Integer id, String logradouro,
+                    String numero, String complemento, String bairro, String cep, Cliente cliente, Cidade cidade) {
+        this.id = id;
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cep = cep;
+        this.cliente = cliente;
+        this.cidade = cidade;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Endereco)) return false;
+        Endereco endereco = (Endereco) o;
+        return Objects.equals(getId(), endereco.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
