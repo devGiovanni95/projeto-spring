ae1d55da218b2daf47f5ce1a6aed67ff1fedff63
devGiovanni95 <giovanni.DevJava@hotmail.com>
2021-07-31 15:07:35 -0300

    Criando classes de ItemPedido e ItemPedidoPK para fazer a inteligacao de Pedido com  Produto

--- src/main/java/br/com/projetospring/entities/Pedido.java
+++ src/main/java/br/com/projetospring/entities/Pedido.java
@@ -3,7 +3,9 @@ package br.com.projetospring.entities;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 public class Pedido implements Serializable {
@@ -29,6 +31,9 @@ public class Pedido implements Serializable {
     @ManyToOne
     @JoinColumn(name = "endereco_de_entrega_id")
     private Endereco enderecoDeEntrega;
+
+    /*Set para nao ter itens repetidos*/
+    private Set<ItemPedido> itens = new HashSet<>();
 
     public Pedido(){
 
@@ -80,6 +85,14 @@ public class Pedido implements Serializable {
 
     public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
         this.enderecoDeEntrega = enderecoDeEntrega;
+    }
+
+    public Set<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public void setItens(Set<ItemPedido> itens) {
+        this.itens = itens;
     }
 
     @Override
