19bf79d7620a786c566866038688f162d6c896f7
devGiovanni95 <giovanni.DevJava@hotmail.com>
2021-08-05 20:13:52 -0300

    Colocando um tratamento nas configurações da classe de Desenvolvimento para tratar uma condicao em que se a chave do Spring for diferente de create ele nao instancia um novo banco

--- src/main/java/br/com/projetospring/config/DevConfig.java
+++ src/main/java/br/com/projetospring/config/DevConfig.java
@@ -2,6 +2,7 @@ package br.com.projetospring.config;
 
 import br.com.projetospring.services.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,8 +16,15 @@ public class DevConfig {
     @Autowired
     private DBService dbService;
 
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
     @Bean
     public boolean instantiateDatabase() throws ParseException {
+
+        if (!"create".equals(strategy)){
+            return false;
+        }
         dbService.instantiateTestDatabase();
         return true;
     }
