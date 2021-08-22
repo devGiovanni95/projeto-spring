9c5c17a91d292554d85ae6279d8beda1ae8f3256
devGiovanni95 <giovanni.DevJava@hotmail.com>
2021-08-22 15:54:45 -0300

    Salvando primeiro arquivo no s3

--- src/main/java/br/com/projetospring/ProjetoSpringApplication.java
+++ src/main/java/br/com/projetospring/ProjetoSpringApplication.java
@@ -1,11 +1,16 @@
 package br.com.projetospring;
 
+import br.com.projetospring.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjetoSpringApplication implements CommandLineRunner {
+
+    @Autowired
+    private S3Service s3Service;
 
 
     public static void main(String[] args) {
@@ -14,6 +19,6 @@ public class ProjetoSpringApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
+        s3Service.uploadFile("C:\\ProjetosJava\\recursos aula spring\\imagensPerfil\\4.jpg");
     }
 }
